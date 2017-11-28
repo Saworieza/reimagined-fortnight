eec5059bf639cfdcaf9114a1b3c9ebe923887c34
Saworieza <saworieza@gmail.com>
2017-11-28 11:57:17 +0300

    s3 env variables

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-AWS.config(
-  :access_key_id => ENV['AKIAIL6A3U5P6KVS7BYA'],
-  :secret_access_key => ENV['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']
-)
-
-S3_BUCKET =  AWS::S3.new.buckets[ENV['matatugalore']]
